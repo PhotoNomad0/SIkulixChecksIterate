05bbd3e1a5ebbe211aae0431f6e23b4e5ac8ae25
blm <mcleanb@photo-nomad.net>
2022-10-03 10:03:18 -0400

    added start.bat to launch app and redirect output to log file

--- start.bat
+++ start.bat
@@ -1,4 +1,12 @@
 echo "Starting test in %CD%"
 
-java -jar %HOMEPATH%\Development\SikulixIDE\sikulixide-2.0.5.jar -c -r %CD%
+rem get current date
+set current=%date%
+set cleanDate=%current: =_%
+set cleanDate=%cleanDate:/=_%
+set logFile=./log/%cleanDate%.log
+echo "logFile is set to = %logFile%"
 
+1>>%logFile% 2>&1 (
+  java -jar %HOMEPATH%\Development\SikulixIDE\sikulixide-2.0.5.jar -c -r %CD%
+)
